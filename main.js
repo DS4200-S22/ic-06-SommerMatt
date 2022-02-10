e2bca4518dd25526d045ac457a888d5144a0fd4f
SommerMatt <sommer.m@northeastern.edu>
2022-02-10 15:23:55 -0500

    ic6

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /*
 
 In-class activity 07 starter code
-Prof. Mosca 
-Modified: 12/06/21 
+Prof. Mosca
+Modified: 12/06/21
 
 */
 
@@ -10,5 +10,53 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let newText = "Random number: "
+    let rand = Math.random();
+    let concat = newText + rand;
+
+    let button = document.getElementById("random_num") // or "random_num"
+    button.innerHTML = concat;
+
+    console.log("Button 1 was clicked");
 }
+
+let counter = true;
+
+function button2Clicked() {
+
+    let shape = document.getElementById("my_circ")
+    if (counter) {
+        shape.style.fill = "green";
+    } else {
+        shape.style.fill = "blue";
+    }
+
+    counter = !counter;
+    console.log(counter)
+    console.log(shape.style.fill)
+}
+/*
+
+// Notes from in-class demo
+
+let season = "winter";
+console.log("hello world");
+console.log(season);
+
+
+
+let random = Math.random()
+console.log(random)
+
+function printBiggerRandom() {
+    let rand1 = Math.random() * 100;
+    console.log(rand1)
+}
+printBiggerRandom()
+
+function nameNumber(name, num) {
+    let str = name + "'s favorite number is " + num
+    return str
+}
+console.log(nameNumber("matt", 17))
+*/
